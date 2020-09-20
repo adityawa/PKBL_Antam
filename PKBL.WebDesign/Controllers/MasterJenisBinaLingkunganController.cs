58942d8b71d6732a76c9a964d61e014d062d94f0
hendrik.sudianto <hendrik.sudianto@gmail.com>
2020-09-21 00:19:16 +0700

    Web Design

---
+++ PKBL.WebDesign/Controllers/MasterJenisBinaLingkunganController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PKBL.WebDesign.Controllers
+{
+    public class MasterJenisBinaLingkunganController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
